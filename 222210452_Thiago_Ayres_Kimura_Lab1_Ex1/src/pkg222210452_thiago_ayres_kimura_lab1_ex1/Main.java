357a40304031485dac9752a4908fac1d5f7f243a
Thiago Kimura <thiagoayreskimura@gmail.com>
2022-08-23 15:25:08 -0300

    lab 1

--- 222210452_Thiago_Ayres_Kimura_Lab1_Ex1/src/pkg222210452_thiago_ayres_kimura_lab1_ex1/Main.java
+++ 222210452_Thiago_Ayres_Kimura_Lab1_Ex1/src/pkg222210452_thiago_ayres_kimura_lab1_ex1/Main.java
@@ -6,46 +6,46 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner (System.in);
-        
-        int numLado;
-        int i;
-        int espaco;
-        
-        
-        System.out.print("Digite o numero de lados\n");
-        
-        numLado = input.nextInt();
-        
-        if(numLado > 1 && numLado<=20){
-            //Parte de Cima
-            for( i=0; i<numLado ; i++){
-                System.out.print("*");
+        try (Scanner input = new Scanner (System.in)) {
+            int numLado;
+            int i;
+            int espaco;
+            
+            
+            System.out.print("Digite o numero de lados\n");
+            
+            numLado = input.nextInt();
+            
+            if(numLado > 1 && numLado<=20){
+                //Parte de Cima
+                for( i=0; i<numLado ; i++){
+                    System.out.print("*");
+                    
+                }
+                //Lado esquerdo
+                for(i=0;i<numLado-2;i++){
+                    System.out.print("\n*");
+                    for(espaco=0;espaco<numLado-2;espaco++){
+                        System.out.print(" ");
+                    }
+                    
+                    System.out.print("*");
+                }
+
                 
-            }
-            //Lado esquerdo
-            for(i=0;i<numLado-2;i++){
-                System.out.print("\n*");
-                for(espaco=0;espaco<numLado-2;espaco++){
-                    System.out.print(" ");
+                
+                System.out.print("\n");
+                
+                //Parte de Baixo
+                for( i=0; i<numLado ; i++){
+                    System.out.print("*");
+                    
                 }
                 
-                System.out.print("*");
             }
-
-            
-            
-            System.out.print("\n");
-            
-            //Parte de Baixo
-            for( i=0; i<numLado ; i++){
-                System.out.print("*");
-                
+            else{
+                System.out.print("Numero invalido.");
             }
-            
-        }
-        else{
-            System.out.print("Numero invalido.");
         }
         
         
